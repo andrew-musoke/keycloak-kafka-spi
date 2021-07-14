ff15469a3200fc1bb7f6ac5573fe231c91b92a52
Musoke Andrew <andrew.musoke@oltranz.com>
2021-07-14 04:36:13 +0300

    edit log object

--- src/main/java/io/github/akoserwal/KeycloakCustomEventListener.java
+++ src/main/java/io/github/akoserwal/KeycloakCustomEventListener.java
@@ -3,19 +3,41 @@ package io.github.akoserwal;
 import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.admin.AdminEvent;
+import twitter4j.JSONObject;
 
 public class KeycloakCustomEventListener implements EventListenerProvider {
 
 	@Override
 	public void onEvent(Event event) {
 			System.out.println("Publishing Login Event:-"+event.getType());
-			Producer.publishEvent("login.keycloak.events", event.getUserId());
+			// build log
+			String userLogString = new JSONObject()
+						.put( "time", event.getTime()) 
+						.put( "eventType", event.getType())  
+						.put( "clientId", event.getClientId())  
+						.put( "userId", event.getUserId())  
+						.put( "ipAddress", event.getIpAddress())  
+						.put( "details", event.getDetails())
+						.put( "error", event.getError())
+						.toString();
+
+			Producer.publishEvent("login.keycloak.events", userLogString);
 	}
 
 	@Override
 	public void onEvent(AdminEvent adminEvent, boolean b) {
 			System.out.println("Publishing Admin Event:-"+adminEvent.getResourceType().name()); 
-			Producer.publishEvent("admin.keycloak.events", adminEvent.getAuthDetails().getUserId());
+
+			//build log
+			String adminLogString = new JSONObject()
+						.put("time", adminEvent.getTime())
+						.put("operationType", adminEvent.getOperationType())
+						.put("resourceType", adminEvent.getResourceType())
+						.put("details", adminEvent.getAuthDetails())
+						.put( "error", adminEvent.getError())
+						.toString();		
+
+			Producer.publishEvent("admin.keycloak.events", adminLogString);
 	}
 
 	@Override
